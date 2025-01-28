59b7a28329b23ba0ccdb0e151c819b94cab90fc7
Thomas Verheyden <80472867+ThomasVrhydn@users.noreply.github.com>
2025-01-28 20:03:29 +0100

    Create gMSA-test.ps1

---
+++ gMSA-test.ps1
@@ -0,0 +1,17 @@
+
+<#
+Wait for new Kerberos ticket to be issued. Kerberos tickets are normally valid for 10 hours.
+
+Reboot the server. When the server is rebooted, a new Kerberos ticket is requested with the new group membership.
+
+Purge the existing Kerberos tickets. This forces the domain controller to request a new Kerberos ticket.
+
+To purge the tickets, from an administrator command prompt on the domain controller, run the following command: klist purge -li 0x3e7
+#>
+
+
+#Install managemed service account on every domain controller
+Write-Host "Install and test" $GMSA -ForegroundColor Green
+Install-ADServiceAccount $GMSA
+Test-ADServiceAccount $GMSA
+ 
